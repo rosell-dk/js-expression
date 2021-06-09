e3950917e04fc61ff89c3cd2d564f0906536dfe6
rosell-dk <bjorn@rosell.dk>
2021-06-09 11:24:03 +0200

    Added Evaluator and tests

---
+++ src/Evaluator.js
@@ -0,0 +1,96 @@
+import { Tokenizer, LITERAL, FUNCTION_CALL_NO_ARGS }  from './Tokenizer.js'
+
+export class Evaluator {
+
+  static ops = {
+    ',': (a,b) => {
+      if (a.forEach) {
+        a.push(b);
+        return a;
+      } else {
+        return [a,b];
+      }
+    },
+    '??': (a, b) => a ?? b,
+    '||': (a, b) => a || b,
+    '&&': (a, b) => a && b,
+    '|': (a, b) => a | b,
+    '^': (a, b) => a ^ b,
+    '&': (a, b) => a & b,
+    '==': (a, b) => a == b,
+    '!=': (a, b) => a != b,
+    '===': (a, b) => a === b,
+    '!==': (a, b) => a !== b,
+    '<': (a, b) => a < b,
+    '>': (a, b) => a > b,
+    '<=': (a, b) => a <= b,
+    '>=': (a, b) => a >= b,
+    '<<': (a, b) => a << b,
+    '>>': (a, b) => a >> b,
+    '>>>': (a, b) => a >>> b,
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+    '%': (a, b) => a % b,
+    '**': (a, b) => a ** b,
+    '!': (a) => !a,
+    '~': (a) => ~a,
+  };
+
+  static functions = {
+    'inArray': (arr, value) => b.inArray(a),
+  }
+
+  static addFunction(functionName, f) {
+    Evaluator.functions[functionName] = f;
+  }
+
+  /**
+   *
+   *
+   */
+  static evaluate(rpnTokens) {
+    //console.log('evaluateRpn', rpnTokens);
+    //console.log('evaluateRpn', rpnTokens.map(function(a) {return a[1]}));
+    let stack = [];
+    let a,b;
+    for (let i=0; i<rpnTokens.length; i++) {
+      let token = rpnTokens[i];
+      let tokenType = token[0];
+      let tokenValue = token[1];
+      if (tokenType == LITERAL) {
+        stack.push(tokenValue)
+      } else if (Tokenizer.isInfix(token)) {
+        b=stack.pop();
+        a=stack.pop();
+        let result = Evaluator.ops[tokenValue](a, b);
+        stack.push(result);
+        //console.log('Performed infix op:', a, b, tokenValue, 'result:', result, 'stack:', stack);
+      } else if (Tokenizer.isPrefix(token)) {
+        a=stack.pop();
+        let result = Evaluator.ops[tokenValue](a);
+        stack.push(result);
+        //console.log('Performed prefix op:', a, tokenValue, 'result:', result, 'stack:', stack);
+      } else if (Tokenizer.isFunctionCall(token)) {
+        let functionName = token[1];
+        if (token[0] == FUNCTION_CALL_NO_ARGS) {
+          stack.push(Evaluator.functions[functionName]());
+        } else {
+          let popped = stack.pop();
+          let arr = [];
+          if (Array.isArray(popped)) {
+            arr = popped;
+          } else {
+            arr.push(popped);
+          }
+          stack.push(Evaluator.functions[functionName](... arr));
+
+        }
+      }
+    }
+    //console.log('result', stack);
+    return stack[0];
+  }
+
+}
---
+++ test/evaluator.test.js
@@ -0,0 +1,78 @@
+import { Tokenizer }  from '../src/Tokenizer.js'
+import { Parser }  from '../src/Parser.js'
+import { Evaluator }  from '../src/Evaluator.js'
+
+import assert from 'assert'
+
+describe('Basic evaluation', () => {
+  let tests = [
+    ['2*3*2', 12],
+    ['2**3', 8],
+    ['3%2', 1],
+    ['4/2', 2],
+    ['6/3%2', 0],
+    ['true==false', false],
+    ['true!=false', true],
+    ['true===true', true],
+    ['8===8===true', true],
+    ['true!==true', false],
+    ['null??2', 2],
+    ['true||false&&true', true],
+    ['2|1', 3],
+    ['2&1', 0],
+    ['12^9', 5],
+    ['1+(2+3)*4', 21],
+    ['2<<1', 4],
+    ['2>>1', 1],
+    ['2>>>1', 1],
+    ['2>2', false],
+    ['2>=2', true],
+    ['2**3**2', 512],
+    ['!false', true],
+    ['~1', -2],
+    ['1,2', [1,2]],
+    ['1,2,3', [1,2,3]],
+  ];
+
+  tests.forEach(arr => {
+    let s = arr[0];
+    let expectedResult = arr[1];
+
+    let tokens = Tokenizer.tokenize(s);
+    let tokensRpn = Parser.parseTokens(tokens);
+    let result = Evaluator.evaluate(tokensRpn);
+
+    it(s + ' => ' + JSON.stringify(expectedResult), () => {
+      assert.deepEqual(result, expectedResult);
+    });
+  });
+});
+
+describe('Custom functions', () => {
+
+  Evaluator.addFunction('equals', (a, b) => (a==b));
+  Evaluator.addFunction('double', (a) => a*2);
+  Evaluator.addFunction('seven', () => 7);
+
+  let tests = [
+    ['equals(1,3)', false],
+    ['equals(1,1)', true],
+    ['double(3)', 6],
+    ['3+double(3)', 9],
+    ['seven()', 7],
+    ['7+seven()', 14],
+  ];
+
+  tests.forEach(arr => {
+    let s = arr[0];
+    let expectedResult = arr[1];
+
+    let tokens = Tokenizer.tokenize(s);
+    let tokensRpn = Parser.parseTokens(tokens);
+    let result = Evaluator.evaluate(tokensRpn);
+
+    it(s + ' => ' + JSON.stringify(expectedResult), () => {
+      assert.deepEqual(result, expectedResult);
+    });
+  });
+});
