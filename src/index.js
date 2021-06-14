d500b66f285590170a61ac07c02593ae86c61c59
rosell-dk <bjorn@rosell.dk>
2021-06-14 19:24:22 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Tokenizer, FUNCTION_CALL, FUNCTION_CALL_NO_ARGS, LITERAL, INFIX_OP, PREFIX_OP, IDENTIFIER, GROUPING_BEGIN, GROUPING_END }  from './Tokenizer.js'
 import { Parser }  from './Parser.js'
 import { Evaluator }  from './Evaluator.js'
-import { Expression }  from './Expression.js'
+import { JsExpression }  from './JsExpression.js'
 
-export { Expression, Tokenizer, Evaluator, Parser };
+export { JsExpression, Tokenizer, Evaluator, Parser };
+export default JsExpression;
